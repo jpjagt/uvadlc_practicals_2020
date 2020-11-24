69b4bf32678d89a71dd4040d20beb7d97e08f02a
jeroen <jeroenpjagt@gmail.com>
2020-11-24 21:55:27 +0100

    add broken lstm code

--- assignment_2/2_recurrentnns_gnns/code/Part 1/lstm.py
+++ assignment_2/2_recurrentnns_gnns/code/Part 1/lstm.py
@@ -7,28 +7,97 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import torch
 import torch.nn as nn
+import torch.nn.functional as F
+
+
+def init_param(size, init="kaiming_normal_"):
+    param = torch.empty(*size)
+    return nn.Parameter(getattr(nn.init, init)(param))
+
+
+class LSTMInternalLayer(nn.Module):
+    def __init__(self, dim_x, dim_h, dim_out, activation_fn):
+        super(LSTMInternalLayer, self).__init__()
+
+        self.weights_x = init_param(size=(dim_out, dim_x))
+        self.weights_h = init_param(size=(dim_out, dim_h))
+        self.bias = init_param(size=(dim_out,), init="zeros_")
+        self.activation_fn = activation_fn
+
+    def forward(self, x, h):
+        return self.activation_fn(
+            torch.matmul(self.weights_x, x.T)
+            + torch.matmul(self.weights_h, h.T)
+            + self.bias
+        )
 
 
 class LSTM(nn.Module):
-
-    def __init__(self, seq_length, input_dim, hidden_dim, num_classes,
-                 batch_size, device):
+    def __init__(
+        self,
+        seq_length,
+        input_dim,
+        hidden_dim,
+        num_classes,
+        batch_size,
+        device,
+    ):
 
         super(LSTM, self).__init__()
-        ########################
-        # PUT YOUR CODE HERE  #
-        #######################
-        raise NotImplementedError
-        ########################
-        # END OF YOUR CODE    #
-        #######################
+
+        self.__embedding_dim = 2
+        self.__num_input_digits = 3
+
+        self.__dim_x = self.__embedding_dim
+        self.__dim_h = hidden_dim
+        self.__dim_out = hidden_dim
+        self.__batch_size = batch_size
+
+        self.g = LSTMInternalLayer(
+            self.__dim_x, self.__dim_h, self.__dim_out, activation_fn=F.tanh
+        )
+        self.i = LSTMInternalLayer(
+            self.__dim_x, self.__dim_h, self.__dim_out, activation_fn=F.sigmoid
+        )
+        self.f = LSTMInternalLayer(
+            self.__dim_x, self.__dim_h, self.__dim_out, activation_fn=F.sigmoid
+        )
+        self.o = LSTMInternalLayer(
+            self.__dim_x, self.__dim_h, self.__dim_out, activation_fn=F.sigmoid
+        )
+
+        self.weights_p = init_param(size=(num_classes, self.__dim_h)).T
+        self.bias_p = init_param(size=(num_classes,), init="zeros_")
+
+        self.embedding = nn.Embedding(
+            self.__num_input_digits, self.__embedding_dim
+        )
+
+        self.to(device)
 
     def forward(self, x):
-        ########################
-        # PUT YOUR CODE HERE  #
-        #######################
-        raise NotImplementedError
-        ########################
-        # END OF YOUR CODE    #
-        #######################
+        prev_h = torch.zeros(self.__batch_size, self.__dim_h)
+        prev_c = torch.zeros(self.__batch_size, self.__dim_h)
+
+        x = x.long().squeeze()
+
+        if len(x.size()) < 2:
+            x = x.view(-1, 1)
+
+        for t in range(x.size()[1]):
+            x_t_embedded = self.embedding(x[:, t].squeeze())
+
+            g = self.g(x_t_embedded, prev_h)
+            i = self.i(x_t_embedded, prev_h)
+            f = self.f(x_t_embedded, prev_h)
+            o = self.o(x_t_embedded, prev_h)
+            c = g * i + prev_c * f
+            h = F.tanh(c) * o
+
+            prev_h = h
+            prev_c = c
+
+            p = torch.matmul(h, self.weights_p) + self.bias_p
+        return F.log_softmax(p)
--- assignment_2/2_recurrentnns_gnns/code/Part 1/train.py
+++ assignment_2/2_recurrentnns_gnns/code/Part 1/train.py
@@ -37,98 +37,146 @@ from peep_lstm import peepLSTM
 
 import numpy as np
 
+import matplotlib.pyplot as plt
+from pathlib import Path
+from collections import defaultdict
+
 # You may want to look into tensorboardX for logging
 # from tensorboardX import SummaryWriter
 
 ###############################################################################
 
+# define results globally to access them in plot_results
+losses = defaultdict(list)
+accs = defaultdict(list)
+timestamp = int(datetime.now().timestamp())
 
-def train(config):
-    np.random.seed(0)
-    torch.manual_seed(0)
 
+def plot_result(result, name, ax):
+    np_result = np.array(list(result.values()))
+    avg_result = np_result.mean(axis=0)
+    std_dev = np.std(np_result, axis=0)
+    ax.plot(avg_result)
+    ax.fill_between(
+        range(np_result.shape[1]),
+        avg_result - std_dev,
+        avg_result + std_dev,
+        alpha=0.5,
+    )
+    ax.set_ylabel(name)
+    ax.set_xlabel("training step")
+
+
+def plot_results(config):
+    fig, axs = plt.subplots(2, 1, figsize=(12, 10))
+    plot_result(losses, "loss", axs[0])
+    plot_result(accs, "accuracy", axs[1])
+    Path("./plots").mkdir(exist_ok=True)
+    plt.suptitle(
+        f"loss and accuracy of {config.model_type} on {config.dataset} dataset"
+    )
+    plt.savefig(f"plots/{timestamp}_train_results.png", bbox_inches="tight")
+
+
+def train_for_seed(config, seed):
+    print(f"training with seed {seed}")
+    np.random.seed(seed)
+    torch.manual_seed(seed)
 
     # Initialize the device which to run the model on
     device = torch.device(config.device)
     print(device)
 
     # Load dataset
-    if config.dataset == 'randomcomb':
-        print('Load random combinations dataset ...')
+    if config.dataset == "randomcomb":
+        print("Load random combinations dataset ...")
         # Initialize the dataset and data loader
         config.num_classes = config.input_length
         dataset = datasets.RandomCombinationsDataset(config.input_length)
-        data_loader = DataLoader(dataset, config.batch_size, num_workers=1,
-                                 drop_last=True)
+        data_loader = DataLoader(
+            dataset, config.batch_size, num_workers=1, drop_last=True
+        )
 
-    elif config.dataset == 'bss':
-        print('Load bss dataset ...')
+    elif config.dataset == "bss":
+        print("Load bss dataset ...")
         # Initialize the dataset and data loader
         config.num_classes = 2
         config.input_dim = 3
         dataset = datasets.BaumSweetSequenceDataset(config.input_length)
-        data_loader = DataLoader(dataset, config.batch_size, num_workers=1,
-                                 drop_last=True)
+        data_loader = DataLoader(
+            dataset, config.batch_size, num_workers=1, drop_last=True
+        )
 
         config.input_length = 4 * config.input_length
 
-    elif config.dataset == 'bipalindrome':
-        print('Load binary palindrome dataset ...')
+    elif config.dataset == "bipalindrome":
+        print("Load binary palindrome dataset ...")
         # Initialize the dataset and data loader
         config.num_classes = config.input_length
         dataset = datasets.BinaryPalindromeDataset(config.input_length)
-        data_loader = DataLoader(dataset, config.batch_size, num_workers=1,
-                                 drop_last=True)
+        data_loader = DataLoader(
+            dataset, config.batch_size, num_workers=1, drop_last=True
+        )
 
-        config.input_length = config.input_length*4+2-1
-
-
+        config.input_length = config.input_length * 4 + 2 - 1
 
     # Setup the model that we are going to use
-    if config.model_type == 'LSTM':
+    if config.model_type == "LSTM":
         print("Initializing LSTM model ...")
         model = LSTM(
-            config.input_length, config.input_dim,
-            config.num_hidden, config.num_classes,
-            config.batch_size, device
+            config.input_length,
+            config.input_dim,
+            config.num_hidden,
+            config.num_classes,
+            config.batch_size,
+            device,
         ).to(device)
 
-    elif config.model_type == 'biLSTM':
+    elif config.model_type == "biLSTM":
         print("Initializing bidirectional LSTM model...")
         model = biLSTM(
-            config.input_length, config.input_dim,
-            config.num_hidden, config.num_classes,
-            config.batch_size, device
+            config.input_length,
+            config.input_dim,
+            config.num_hidden,
+            config.num_classes,
+            config.batch_size,
+            device,
         ).to(device)
 
-    elif config.model_type == 'GRU':
+    elif config.model_type == "GRU":
         print("Initializing GRU model ...")
         model = GRU(
-            config.input_length, config.input_dim,
-            config.num_hidden, config.num_classes,
-            config.batch_size, device
+            config.input_length,
+            config.input_dim,
+            config.num_hidden,
+            config.num_classes,
+            config.batch_size,
+            device,
         ).to(device)
 
-    elif config.model_type == 'peepLSTM':
+    elif config.model_type == "peepLSTM":
         print("Initializing peephole LSTM model ...")
         model = peepLSTM(
-            config.input_length, config.input_dim,
-            config.num_hidden, config.num_classes,
-            config.batch_size, device
+            config.input_length,
+            config.input_dim,
+            config.num_hidden,
+            config.num_classes,
+            config.batch_size,
+            device,
         ).to(device)
 
     # Setup the loss and optimizer
-    loss_function = torch.nn.NLLLoss()
+    # loss_function = torch.nn.NLLLoss()
+    loss_function = torch.nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=config.learning_rate)
 
     for step, (batch_inputs, batch_targets) in enumerate(data_loader):
-
         # Only for time measurement of step through network
         t1 = time.time()
 
         # Move to GPU
-        batch_inputs = batch_inputs.to(device)     # [batch_size, seq_length,1]
-        batch_targets = batch_targets.to(device)   # [batch_size]
+        batch_inputs = batch_inputs.to(device)  # [batch_size, seq_length,1]
+        batch_targets = batch_targets.to(device)  # [batch_size]
 
         # Reset for next iteration
         model.zero_grad()
@@ -143,8 +191,9 @@ def train(config):
         #######################################################################
         # Check for yourself: what happens here and why?
         #######################################################################
-        torch.nn.utils.clip_grad_norm_(model.parameters(),
-                                       max_norm=config.max_norm)
+        torch.nn.utils.clip_grad_norm_(
+            model.parameters(), max_norm=config.max_norm
+        )
         #######################################################################
 
         optimizer.step()
@@ -157,17 +206,26 @@ def train(config):
 
         # Just for time measurement
         t2 = time.time()
-        examples_per_second = config.batch_size/float(t2-t1)
+        examples_per_second = config.batch_size / float(t2 - t1)
 
         if step % 60 == 0:
 
-            print("[{}] Train Step {:04d}/{:04d}, Batch Size = {}, \
+            print(
+                "[{}] Train Step {:04d}/{:04d}, Batch Size = {}, \
                    Examples/Sec = {:.2f}, "
-                  "Accuracy = {:.2f}, Loss = {:.3f}".format(
-                    datetime.now().strftime("%Y-%m-%d %H:%M"), step,
-                    config.train_steps, config.batch_size, examples_per_second,
-                    accuracy, loss
-                    ))
+                "Accuracy = {:.2f}, Loss = {:.3f}".format(
+                    datetime.now().strftime("%Y-%m-%d %H:%M"),
+                    step,
+                    config.train_steps,
+                    config.batch_size,
+                    examples_per_second,
+                    accuracy,
+                    loss,
+                )
+            )
+
+        losses[str(seed)].append(loss.item())
+        accs[str(seed)].append(accuracy)
 
         # Check if training is finished
         if step == config.train_steps:
@@ -175,9 +233,12 @@ def train(config):
             # https://github.com/pytorch/pytorch/pull/9655
             break
 
-    print('Done training.')
-    ###########################################################################
-    ###########################################################################
+
+def train(config):
+    for seed in [0, 42, 84]:
+        train_for_seed(config, seed)
+    print("Done training.")
+    plot_results(config)
 
 
 if __name__ == "__main__":
@@ -186,42 +247,95 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     # dataset
-    parser.add_argument('--dataset', type=str, default='randomcomb',
-                        choices=['randomcomb', 'bss', 'bipalindrome'],
-                        help='Dataset to be trained on.')
+    parser.add_argument(
+        "--dataset",
+        type=str,
+        default="randomcomb",
+        choices=["randomcomb", "bss", "bipalindrome"],
+        help="Dataset to be trained on.",
+    )
     # Model params
-    parser.add_argument('--model_type', type=str, default='biLSTM',
-                        choices=['LSTM', 'biLSTM', 'GRU', 'peepLSTM'],
-                        help='Model type: LSTM, biLSTM, GRU or peepLSTM')
-    parser.add_argument('--input_length', type=int, default=6,
-                        help='Length of an input sequence')
-    parser.add_argument('--input_dim', type=int, default=1,
-                        help='Dimensionality of input sequence')
-    parser.add_argument('--num_classes', type=int, default=1,
-                        help='Dimensionality of output sequence')
-    parser.add_argument('--num_hidden', type=int, default=256,
-                        help='Number of hidden units in the model')
+    parser.add_argument(
+        "--model_type",
+        type=str,
+        default="biLSTM",
+        choices=["LSTM", "biLSTM", "GRU", "peepLSTM"],
+        help="Model type: LSTM, biLSTM, GRU or peepLSTM",
+    )
+    parser.add_argument(
+        "--input_length",
+        type=int,
+        default=6,
+        help="Length of an input sequence",
+    )
+    parser.add_argument(
+        "--input_dim",
+        type=int,
+        default=1,
+        help="Dimensionality of input sequence",
+    )
+    parser.add_argument(
+        "--num_classes",
+        type=int,
+        default=1,
+        help="Dimensionality of output sequence",
+    )
+    parser.add_argument(
+        "--num_hidden",
+        type=int,
+        default=256,
+        help="Number of hidden units in the model",
+    )
 
     # Training params
-    parser.add_argument('--batch_size', type=int, default=256,
-                        help='Number of examples to process in a batch')
-    parser.add_argument('--learning_rate', type=float, default=0.001,
-                        help='Learning rate')
-    parser.add_argument('--train_steps', type=int, default=3000,
-                        help='Number of training steps')
-    parser.add_argument('--max_norm', type=float, default=10.0)
+    parser.add_argument(
+        "--batch_size",
+        type=int,
+        default=256,
+        help="Number of examples to process in a batch",
+    )
+    parser.add_argument(
+        "--learning_rate", type=float, default=0.001, help="Learning rate"
+    )
+    parser.add_argument(
+        "--train_steps",
+        type=int,
+        default=3000,
+        help="Number of training steps",
+    )
+    parser.add_argument("--max_norm", type=float, default=10.0)
 
     # Misc params
-    parser.add_argument('--device', type=str, default="cuda:0",
-                        help="Training device 'cpu' or 'cuda:0'")
-    parser.add_argument('--gpu_mem_frac', type=float, default=0.5,
-                        help='Fraction of GPU memory to allocate')
-    parser.add_argument('--log_device_placement', type=bool, default=False,
-                        help='Log device placement for debugging')
-    parser.add_argument('--summary_path', type=str, default="./summaries/",
-                        help='Output path for summaries')
+    parser.add_argument(
+        "--device",
+        type=str,
+        default="cuda:0",
+        help="Training device 'cpu' or 'cuda:0'",
+    )
+    parser.add_argument(
+        "--gpu_mem_frac",
+        type=float,
+        default=0.5,
+        help="Fraction of GPU memory to allocate",
+    )
+    parser.add_argument(
+        "--log_device_placement",
+        type=bool,
+        default=False,
+        help="Log device placement for debugging",
+    )
+    parser.add_argument(
+        "--summary_path",
+        type=str,
+        default="./summaries/",
+        help="Output path for summaries",
+    )
 
     config = parser.parse_args()
 
-    # Train the model
-    train(config)
+    try:
+        # Train the model
+        print(f"timestamp: {timestamp}")
+        train(config)
+    except KeyboardInterrupt:
+        plot_results(config)
